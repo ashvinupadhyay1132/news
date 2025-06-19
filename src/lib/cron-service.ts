845a904a7d4f5f770efc50018da3f374c2f4c369
Ashvin Upadhyay <ashvinupadhyay1132@gmail.com>
2025-06-19 11:33:44 +0530

    add favicon

--- src/lib/cron-service.ts
+++
@@ -1,3 +0,0 @@
-// This file is intentionally blank as the cron service has been removed.
-// Automatic background updates are no longer active.
-// News updates are now handled manually via the admin panel.
